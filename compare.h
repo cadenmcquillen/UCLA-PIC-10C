26d9979cdcc2be8104b3be81190fcab8148717dd
cadenmcquillen <54229205+cadenmcquillen@users.noreply.github.com>
2022-04-13 19:06:51 -0700

    Delete compare.h

--- compare.h
+++
@@ -1,9 +0,0 @@
-#include <filesystem>
-#include <iostream>
-#include <fstream>   
-#include <algorithm> 
-
-#ifndef compare_h
-#define compare_h
-void Longest_Common_Substring (std::string Submission_Folder, std::pair<std::filesystem::path, std::filesystem::path> File_Pair);
-#endif /* compare_h */
